947a9d5813d2e3c273bd5d3f12643a9f6499eee3
taiz <llamttai@gmail.com>
2024-05-14 14:43:21 +0700

    2:43 14/5

--- frontend/src/components/pages/Admin/Management/ManagementStudent/Student.js
+++ frontend/src/components/pages/Admin/Management/ManagementStudent/Student.js
@@ -1,8 +1,9 @@
 // Student.js
 
-import { useEffect, useState } from "react";
-import { Button, Select, Tooltip } from 'antd';
-import { Link } from "react-router-dom";
+import React, { useEffect, useState, useRef } from "react";
+import { Button, Select, Tooltip, Input, Space, Table } from 'antd';
+import { Link, useLocation } from "react-router-dom";
+import { SearchOutlined } from '@ant-design/icons';
 import './Student.css'
 
 import {
@@ -14,17 +15,8 @@ import {
 } from "@nextui-org/react";
 import { axiosAdmin } from "../../../../../service/AxiosAdmin";
 
-
-//test
-
-
-import React, { useRef } from 'react';
-import { SearchOutlined } from '@ant-design/icons';
-import { Input, Space, Table } from 'antd';
-// import Highlighter from 'react-highlight-words';
-
-//end test
 const Student = (nav) => {
+  const location = useLocation();
   const { setCollapsedNav } = nav;
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
@@ -241,8 +233,8 @@ const Student = (nav) => {
       // filterSearch: true,
       ...getColumnSearchProps('classCode'), //search và filter chỉ ton tai 1 
       width: '30%',
-      sorter: (a, b) => a.classCode.length - b.classCode.length,
-      sortDirections: ['descend', 'ascend'],
+      // sorter: (a, b) => a.classCode - b.classCode,
+      // sortDirections: ['descend', 'ascend'],
     },
   ];
 
@@ -251,150 +243,120 @@ const Student = (nav) => {
   };
 
   return (
-    // <div className="flex w-full flex-col justify-center leading-8 pt-5 bg-[#f5f5f5]-500">
-    //   <ConfirmAction
-    //     onOpenChange={onOpenChange}
-    //     isOpen={isOpen}
-    //     onConfirm={() => {
-    //       if (deleteId) {
-    //         hangleChangeidDelete(deleteId);
-    //         setDeleteId(null);
-    //       }
-    //     }}
-    //   />
-    //   <div>
-    //     <div className="w-fit flex border justify-start text-base font-bold rounded-lg">
-    //       <Link to={"/admin/student"}>
-    //         <div className="p-5 hover:bg-slate-600 hover:text-white">
-    //           DS Sinh viên
-    //         </div>
-    //       </Link>
-    //       <Link to={"/admin/student/store"}>
-    //         <div className="p-5 hover:bg-slate-600 hover:text-white">
-    //           Kho lưu trữ
-    //         </div>
-    //       </Link>
-    //       <Link to={"/admin/student/create"}>
-    //         <div className="p-5 hover:bg-slate-600 hover:text-white">
-    //           Thêm sinh viên
-    //         </div>
-    //       </Link>
-    //       <Link to={"/admin/student/update"}>
-    //         <div className="p-5 hover:bg-slate-600 hover:text-white">
-    //           Cập nhật
-    //         </div>
-    //       </Link>
-    //       {/* <Link to={"/admin/student/po-plo"}>
-    //         <div className="p-5 hover:bg-slate-600 hover:text-white">
-    //           PO-PLO
-    //         </div>
-    //       </Link> */}
-    //     </div>
-    //   </div>
-    //   <Select
-    //     mode="multiple"
-    //     maxCount={1}
-    //     style={{ width: '100%' }}
-    //     placeholder="Chọn mã lớp"
-    //     onChange={(e) => handleClassChange(e)}
-    //     options={classOptions}
-    //   />
-    //   <div className="w-full border mt-5 rounded-lg">
-    //     <table className="table-auto border-collapse border w-full">
-    //       <tr>
-    //         <td className="p-2 border-1 sm:px-4 sm:py-2 lg:px-4 lg:py-2 xl:px-4 xl:py-2 bg-gray-800 text-white text-center">STT</td>
-    //         <td className="p-2 border-1 sm:px-4 sm:py-2 lg:px-4 lg:py-2 xl:px-4 xl:py-2 bg-gray-800 text-white text-center">Họ và tên</td>
-    //         <td className="p-2 border-1 sm:px-4 sm:py-2 lg:px-4 lg:py-2 xl:px-4 xl:py-2 bg-gray-800 text-white text-center">MSSV</td>
-    //         <td className="p-2 border-1 sm:px-4 sm:py-2 lg:px-4 lg:py-2 xl:px-4 xl:py-2 bg-gray-800 text-white text-center">Thao tác</td>
-    //       </tr>
-    //       {data.map((data, i) => (
-    //         <tr key={i}>
-    //           <td className="p-2 border-1 sm:px-4 sm:py-2 lg:px-4 lg:py-2 xl:px-4 xl:py-2">{i + 1}</td>
-    //           <td className="p-2 border-1 sm:px-4 sm:py-2 lg:px-4 lg:py-2 xl:px-4 xl:py-2">{data.name}</td>
-    //           <td className="p-2 border-1 sm:px-4 sm:py-2 lg:px-4 lg:py-2 xl:px-4 xl:py-2">{data.studentCode}</td>
-    //           <td className="p-2 border-1 sm:px-4 sm:py-2 lg:px-4 lg:py-2 xl:px-4 xl:py-2 flex justify-center">
-    //             <div className="flex gap-1 flex-col sm:flex-col lg:flex-row xl:flex-row">
-    //               <div className="bg-blue-500 w-[120px] hover:bg-blue-700 text-white  text-center font-bold p-1 rounded">
-    //                 <Link to={`update/${data.student_id}`} className="w-full h-full">Cập nhật</Link></div>
-    //               <div className="bg-red-500 w-[120px] hover:bg-red-700 text-white text-center font-bold p-1 rounded">
-    //                 <button onClick={() => { onOpen(); setDeleteId(data.student_id); }} className="w-full h-full">Xóa</button>
-    //               </div>
-    //             </div>
-    //           </td>
-    //         </tr>
-    //       ))}
-    //     </table>
-    //   </div>
-    // </div>
-    <Table
-      columns={columns}
-      dataSource={studentData}
-      onChange={onChange}
-      pagination={{
-        defaultPageSize: 10,
-        responsive: true,
-        style: { justifyContent: "center" }
-      }}
-    />
+
+    <>
+      <div className="flex w-full flex-col justify-center leading-8 pt-5 bg-[#f5f5f5]-500">
+        <ConfirmAction
+          onOpenChange={onOpenChange}
+          isOpen={isOpen}
+          onConfirm={() => {
+            if (deleteId) {
+              hangleChangeidDelete(deleteId);
+              setDeleteId(null);
+            }
+          }}
+        />
+        <div>
+          <div className="w-fit flex justify-start text-base font-bold rounded-lg mb-5">
+            <Link to={"/admin/student"} className="rounded-lg bg-blue-600">
+              <div className="p-5 text-white rounded-lg">
+                DS Sinh viên
+              </div>
+            </Link>
+            <Link to={"/admin/student/store"}>
+              <div className="p-5 hover:bg-blue-600 hover:text-white rounded-lg">
+                Kho lưu trữ
+              </div>
+            </Link>
+            <Link to={"/admin/student/create"}>
+              <div className="p-5 hover:bg-blue-600 hover:text-white rounded-lg">
+                Thêm sinh viên
+              </div>
+            </Link>
+            <Link to={"/admin/student/update"}>
+              <div className="p-5 hover:bg-blue-600 hover:text-white rounded-lg">
+                Cập nhật
+              </div>
+            </Link>
+            {/* <Link to={"/admin/student/po-plo"}>
+            <div className="p-5 hover:bg-slate-600 hover:text-white">
+              PO-PLO
+            </div>
+          </Link> */}
+          </div>
+        </div>
+      </div>
+      <Table
+        columns={columns}
+        dataSource={studentData}
+        onChange={onChange}
+        pagination={{
+          defaultPageSize: 10,
+          responsive: true,
+          style: { justifyContent: "center" }
+        }}
+      />
+    </>
+
   );
 }
 
 export default Student;
 
-// function ConfirmAction(props) {
-//   const { isOpen, onOpenChange, onConfirm } = props;
-//   const handleOnOKClick = (onClose) => {
-//     onClose();
-//     console.log('thanđ');
-//     if (typeof onConfirm === 'function') {
-//       onConfirm();
-//     }
-//   }
-//   return (
-//     <Modal
-//       isOpen={isOpen}
-//       onOpenChange={onOpenChange}
-//       motionProps={{
-//         variants: {
-//           enter: {
-//             y: 0,
-//             opacity: 1,
-//             transition: {
-//               duration: 0.2,
-//               ease: "easeOut",
-//             },
-//           },
-//           exit: {
-//             y: -20,
-//             opacity: 0,
-//             transition: {
-//               duration: 0.1,
-//               ease: "easeIn",
-//             },
-//           },
-//         }
-//       }}
-//     >
-//       <ModalContent>
-//         {(onClose) => (
-//           <>
-//             <ModalHeader>Cảnh báo</ModalHeader>
-//             <ModalBody>
-//               <p className="text-[16px]">
-//                 Chương trình sẽ được chuyển vào <Chip radius="sm" className="bg-zinc-200"><i class="fa-solid fa-trash-can-arrow-up mr-2"></i>Kho lưu trữ</Chip> và có thể khôi phục lại, tiếp tục thao tác?
-//               </p>
-//             </ModalBody>
-//             <ModalFooter>
-//               <Button variant="light" onClick={onClose}>
-//                 Huỷ
-//               </Button>
-//               <Button color="danger" className="font-medium" onClick={() => handleOnOKClick(onClose)}>
-//                 Xoá
-//               </Button>
-//             </ModalFooter>
-//           </>
-//         )}
-//       </ModalContent>
-//     </Modal>
-//   )
-// }
+function ConfirmAction(props) {
+  const { isOpen, onOpenChange, onConfirm } = props;
+  const handleOnOKClick = (onClose) => {
+    onClose();
+    console.log('thanđ');
+    if (typeof onConfirm === 'function') {
+      onConfirm();
+    }
+  }
+  return (
+    <Modal
+      isOpen={isOpen}
+      onOpenChange={onOpenChange}
+      motionProps={{
+        variants: {
+          enter: {
+            y: 0,
+            opacity: 1,
+            transition: {
+              duration: 0.2,
+              ease: "easeOut",
+            },
+          },
+          exit: {
+            y: -20,
+            opacity: 0,
+            transition: {
+              duration: 0.1,
+              ease: "easeIn",
+            },
+          },
+        }
+      }}
+    >
+      <ModalContent>
+        {(onClose) => (
+          <>
+            <ModalHeader>Cảnh báo</ModalHeader>
+            <ModalBody>
+              <p className="text-[16px]">
+                Chương trình sẽ được chuyển vào <Chip radius="sm" className="bg-zinc-200"><i class="fa-solid fa-trash-can-arrow-up mr-2"></i>Kho lưu trữ</Chip> và có thể khôi phục lại, tiếp tục thao tác?
+              </p>
+            </ModalBody>
+            <ModalFooter>
+              <Button variant="light" onClick={onClose}>
+                Huỷ
+              </Button>
+              <Button color="danger" className="font-medium" onClick={() => handleOnOKClick(onClose)}>
+                Xoá
+              </Button>
+            </ModalFooter>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
+  )
+}
